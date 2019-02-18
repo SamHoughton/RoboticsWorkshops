1d4e589364986df1142a9be9ae2f343533a25839
Hayden <hayden@haydencoe.co.uk>
2019-02-18 15:55:50 +0000

    Update WheelSpin2.py

--- WheelSpin2.py
+++ WheelSpin2.py
@@ -6,7 +6,7 @@ from std_msgs.msg import Float32
 
 class Control:
 	def __init__(self):
-		self.input_pub = rospy.Publisher('/turtlebot_1/wheel_vel_left', Float32, queue_size=10)
+		self.input_pub = rospy.Publisher('/wheel_vel_left', Float32, queue_size=10)
 		vel = input("")		
 		self.publish_velocity(vel)
 		
