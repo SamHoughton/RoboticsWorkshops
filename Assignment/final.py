50502f29a99b99792930ff06b62552e564047e0c
SamHoughton <43782912+SamHoughton@users.noreply.github.com>
2019-03-21 13:33:11 +0000

    Update final.py

--- Assignment/final.py
+++ Assignment/final.py
@@ -7,8 +7,7 @@ import numpy as maths
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
-# if the service takes a long time to execute, the user might want the ability to cancel the request during execution
-# or get periodic feedback about how the request is progressing.
+# Imports the action library for the users
 import actionlib 
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionFeedback
 
@@ -16,37 +15,33 @@ class findpillar:
 
     def __init__(self):
 
-        # Allows for windows to be opened for the images in the program
+        # Allows for windows to be opened for the images in the program for program to display raw and segmented images
         cv2.startWindowThread()
 
-        # Robots Position to start will be 0, 0 on the centre spot
+        # Robots Position to start will be 0, 0 on the centre spot of the arena of pillars
         self.robotLocalX = 0
         self.robotLocalX = 0
 
-        # This is a flag point to switch when a colour is found
+        # This is a list to switch parts to true when a colour is found
         self.Found = [False, False, False, False]
     
-        
+        # These are the waypoints of the robot to go visit around the map, one for the x and one for the y
         self.wayPointsX = [1.5, 2, 0 , -0.5, 0 , -4.2]
         self.wayPointsY = [-4.4, 0, 0 , -1.5, 1.4 , 1]
+        # These are waypoints sent and waypoints recieved by the robot and will also be switched when found
         self.wayPointsS = [False, False, False, False, False, False]
-        self.wayPointsR = [False, False, False, False, False, False]
-        
-        
-        
+        # Waypoints Recieved
+        self.wayPointsR = [False, False, False, False, False, False]       
+        # provides an interface between the robot images and open CV images
         self.bridge = CvBridge()
-        
-        #Declaring Subscribers
         
         # One for the camera raw image to gather the input from the camera
         self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.imageCallback)
-        # One to gather the input from the move base 
+        # A simple position subscriber to gather information about robot movement
         self.position = rospy.Subscriber("/move_base/feedback", MoveBaseActionFeedback, self.closeWaypoint)
         
-        #Declares Publisher for Velocity Movement
-        
-        self.cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=5)  
-        
+        #Declares the publisher for the movement of the robot
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=5)          
         # Declares the twist command for the robot
         self.twist = Twist()
     
@@ -73,31 +68,38 @@ class findpillar:
           
     def imageCallback(self, data):
         
-        # a conversion method that is protected againsted in case it fails
+        # a conversion method that is protected againsted in case it fails, will print the error if failure occurs
         try:
             cvImage = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError, e:
-            print e
+        except CvBridgeError, error:
+            print error
         
         # the colours for the different thresholds in two arrays to loop through
+        # Each four of the colour boundries are stored here and will needed to be looped through
         coloursLower = [0,60,100,25]
         coloursUpper = [4,70,120,30]
+        # Stores an empty colour mask for the different colours
         colourMask = [None]*4            
         
-        #Creating the HSV Colour Bounds - Looks for Blue
+        # Converts the image into the correct colour type to be displayed
         hsvImage = cv2.cvtColor(cvImage, cv2.COLOR_BGR2HSV)
         
         # From the workshop, declare a variable to store values
         default_low = maths.array([255, 255, 255])
+        # Creates a back up one in case the colours fail
         default_high = maths.array([255, 255, 205])
         
-        hsvThresh = cv2.inRange(hsvImage, default_low, default_high)        
+        # Makes the threshold for the colours using the arrays
+        hsvThresh = cv2.inRange(hsvImage, default_low, default_high)   
+        # Stores the threshold in a moment to ensure it isnt lost
         hsv = cv2.moments(hsvThresh)
 
-        
+        # Groups the colours together 
         coloursTogether = [hsv, hsv, hsv, hsv]
         
+        # For i in the length of the colours found, in this case 4
         for i in range(len(self.Found)):
+            # Loop through the different colour masks and ensure that each colour is passed in
             colourMask[i] = cv2.inRange(hsvImage, maths.array([coloursLower[i], 100, 100]), maths.array([coloursUpper[i], 255, 255])) 
             if self.Found[i] == False:
                 coloursTogether[i] = cv2.moments(colourMask[i])
@@ -110,8 +112,7 @@ class findpillar:
             
         # Adds the colours into one threshold - From the workshops
         hsvThresh = colourMask[0]+colourMask[1]+colourMask[2]+colourMask[3]
-        
-        # Runs the image display functions
+
         self.imageShow(hsvThresh, hsvImage, cvImage)
         
         colourCounter = ''
@@ -120,9 +121,7 @@ class findpillar:
                 # Checks the ampount of the threshold on the screen and if the colour has been found or not 
                 if coloursTogether[i]['m00'] > 800000 and self.Found[i] == False:
                     hsvThresh = colourMask[i]
-                    # Sets the colour counter to the correct setting
                     colourCounter = i
-                    # Passes the information to beacon detection so that the robot can start moving towards it 
                     self.Found[i] = self.BeaconDetect(cvImage, hsvThresh, colourCounter)
                     break
             else:
@@ -156,12 +155,11 @@ class findpillar:
         cv2.waitKey(1)
 
     def BeaconDetect(self, cvImage, hsvThresh, colourCounter):
-       
-       #Creates height, width, depth variables
+        
         height, width, depth = cvImage.shape
-        #Creates a bound for top of shape of the thresholded colour
+        #Top of the shape of the thresholded colour for example red bottom
         topSearch = height*0.5
-        #Creates a bound for top of shape of the thresholded colour
+        #Top of the shape of the thresholded colour for example red top
         botSearch = height*0.8
         # decides how far the robot should be before it finds the pillars
         lim = height*0.7
@@ -172,7 +170,7 @@ class findpillar:
         cancelGoal.cancel_goal()
                 
         
-        # Sets the beacon to found and alerts the user that the beacon has been found
+        # Returns true for the beacon being found and tells which one has been found
         if hsvThresh[lim:height, 0:width].any():
             if colourCounter == 0:
                 print("Found Red!")
@@ -184,7 +182,7 @@ class findpillar:
                 print("Found Yellow!")
             return True        
         
-        # These are boundries so the threshold cant escape it
+        # These threshold boundries keep it from escaping
         hsvThresh[0:topSearch, 0:width] = 0
         hsvThresh[botSearch:height, 0:width] = 0
         
@@ -195,16 +193,13 @@ class findpillar:
         # Stores a moment of the threshold
         if moments['m00'] > 0:
            
-           cx = int(moments['m10']/moments['m00'])
-           
+           cx = int(moments['m10']/moments['m00'])    
            cy = int(moments['m01']/moments['m00'])  
-            #This creates the circle for window image
            cv2.circle(cvImage, (cx, cy), 20, (0, 0, 100), -1)
            error = cx - width/2
-           # moves the robot along the X
+           # moves the robot along the X at the speed of 0.5
            self.twist.linear.x = 0.5
            self.twist.angular.z = -float(error) / 100
-           # Publishes the twist command to the robot
            self.cmd_vel_pub.publish(self.twist)      
 
         return False
@@ -217,7 +212,7 @@ class findpillar:
         target = MoveBaseGoal()
         target.target_pose.header.frame_id = "map"
         target.target_pose.header.stamp = rospy.Time.now()
-        # Specify each of the goal positions
+        # Specify each of the goal positions from the waypoint coordinates
         target.target_pose.pose.position.x = x
         target.target_pose.pose.position.y = y
         target.target_pose.pose.orientation.x = 0.0
@@ -226,7 +221,7 @@ class findpillar:
         if pointNum == 5:
             target.target_pose.pose.orientation.z = -0.5
         target.target_pose.pose.orientation.w = 1.0
-        # Send the waypoint to the navigation stack
+        # Send the waypoint to the navigation stack to the top
         action.send_goal(target)
         
         self.laserS=rospy.Subscriber("/scan", LaserScan, self.laserCallback)
@@ -236,7 +231,7 @@ class findpillar:
             if pointNum == i:
                 self.wayPointsS[i] = True
                 print("Im on my way to waypoint " + str(pointNum+1))
-#           
+#   definition for the close waypoint function        
     def closeWaypoint(self, data):
 
         # Take data from the library and determine where the robot is in the location on the map
@@ -247,7 +242,7 @@ class findpillar:
         self.robotorz = data.feedback.base_position.pose.orientation.z
         error = 0.50     
 
-        # The srttings to insure that the waypoints close and the data is saved
+        # The settings to ensure that the waypoints are set to close and that the next wyapoint can be moved to by the robot
         for i in range(len(self.wayPointsR)):
             if self.wayPointsS[i] == True and self.wayPointsR[i] == False:
                 if self.robotposx >= (self.wayPointsX[i] - error) and self.robotposx <= (self.wayPointsX[i] + error) and self.robotposy >= (self.wayPointsY[i]- error) and self.robotposy <= (self.wayPointsY[i] + error):
